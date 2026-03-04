6b25ec809be77ca806e286b29b6a9a7c434edb30
louiejim <louiejimsky29@gmail.com>
2026-03-04 23:56:12 +0800

    about commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import { OutsystemsIcon } from "../util/Icons";
+import { AlvIcon, SevenSevenIcon } from "../util/Icons";
 
 const About = () => {
   return (
@@ -11,7 +11,7 @@ const About = () => {
       <h1 className="text-4xl font-bold text-center mb-1">About me</h1>
       <div className="flex flex-row ml-26 mb-20">
         <div>
-          <p className="mt-4 text-lg text-gray-300 px-4 md:px-32 flex-1">
+          <p className="mt-4 text-lg text-gray-300 px-4 md:px-5 flex-1">
             Experienced in both traditional coding and low-code software
             development, with a proven track record in the Information
             Technology and Services industry. Proficient in Java (Spring Boot),
@@ -34,19 +34,111 @@ const About = () => {
             contentStyle={{
               background: "rgb(244, 250, 255)",
               color: "black",
+              borderTop: "3px solid #fd4f31",
             }}
             animate={true}
             contentArrowStyle={{ borderRight: "7px solid  rgb(254, 255, 255)" }}
-            date={<span style={{ color: "#ffffff" }}>Nov 2024 – Present</span>}
+            date={<span style={{ color: "#ffffff" }}>July 2025 - Present</span>}
             iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={<OutsystemsIcon />}
+            icon={<SevenSevenIcon />}
           >
-            <h3 className="vertical-timeline-element-title">
+            <h3
+              className="vertical-timeline-element-title"
+              style={{
+                fontSize: "20px",
+                lineHeight: "30px",
+                paddingBottom: "6px",
+                fontWeight: "400px",
+              }}
+            >
+              Application Developer 3
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Seven Seven Global Services, Inc.
+            </h4>
+            <ul className="list-disc pl-6 space-y-2 text-gray-600 mt-5">
+              <li>
+                Create software applications based on project requirements.
+              </li>
+              <li>
+                Take on leadership responsibilities in projects, possibly
+                mentoring junior developers.
+              </li>
+              <li>
+                Work with other developers, designers, and stakeholders to
+                ensure application functionality and user experience.
+              </li>
+              <li>
+                Identify and resolve issues, as well as perform regular
+                maintenance on applications.
+              </li>
+              <li>
+                Adhere to coding standards and best practices in software
+                development.
+              </li>
+            </ul>
+          </VerticalTimelineElement>
+          {/* 0 */}
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{
+              background: "rgb(244, 250, 255)",
+              color: "black",
+              borderTop: "3px solid #fd4f31",
+            }}
+            animate={true}
+            contentArrowStyle={{ borderRight: "7px solid  rgb(254, 255, 255)" }}
+            date={
+              <span style={{ color: "#ffffff" }}>Nov 2024 – July 2025</span>
+            }
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+            icon={<SevenSevenIcon />}
+          >
+            <h3
+              className="vertical-timeline-element-title"
+              style={{
+                fontSize: "20px",
+                lineHeight: "30px",
+                paddingBottom: "6px",
+                fontWeight: "400px",
+              }}
+            >
               Application Developer 2
             </h3>
             <h4 className="vertical-timeline-element-subtitle">
               Seven Seven Global Services, Inc.
             </h4>
+            <ul className="list-disc pl-6 space-y-2 text-gray-600 mt-5">
+              <li>
+                Develops robust full-stack web applications using React
+                (frontend) and Java Spring Boot (backend), with a focus on
+                performance, scalability, and user experience.
+              </li>
+              <li>
+                Builds and consumes RESTful APIs, enabling seamless data
+                communication between client and server.
+              </li>
+              <li>
+                Implements unit and integration tests using JUnit and Mockito,
+                ensuring high code quality and reliability throughout the
+                development lifecycle
+              </li>
+              <li>
+                Applies responsive design principles and state management in
+                React (e.g., using Redux –Saga, Hooks) to create dynamic and
+                interactive user interfaces.
+              </li>
+              <li>
+                Collaborates in an Agile environment with cross-functional
+                teams, participating in sprint planning, daily stand-ups, and
+                code reviews.
+              </li>
+              <li>
+                Manages version control and collaboration using Git, and
+                contributed to CI/CD pipelines for automated testing and
+                deployment.
+              </li>
+            </ul>
           </VerticalTimelineElement>
 
           {/* 1 */}
@@ -55,21 +147,51 @@ const About = () => {
             contentStyle={{
               background: "rgb(244, 250, 255)",
               color: "black",
+              borderTop: "3px solid #fd4f31",
             }}
             animate={true}
             contentArrowStyle={{ borderRight: "7px solid  rgb(254, 255, 255)" }}
             date={
               <span style={{ color: "#ffffff" }}>April 2021 – Nov 2024</span>
             }
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={<OutsystemsIcon />}
+            iconStyle={{ background: "#4cc2e4;", color: "#fff" }}
+            icon={<SevenSevenIcon />}
           >
-            <h3 className="vertical-timeline-element-title">
+            <h3
+              className="vertical-timeline-element-title"
+              style={{
+                fontSize: "20px",
+                lineHeight: "30px",
+                paddingBottom: "6px",
+                fontWeight: "400px",
+              }}
+            >
               Application Developer 1
             </h3>
             <h4 className="vertical-timeline-element-subtitle">
               Seven Seven Global Services, Inc.
             </h4>
+            <ul className="list-disc pl-6 space-y-2 text-gray-600 mt-5">
+              <li>
+                Developed front-end web applications for the client using the
+                OutSystems platform, following industry best practices for
+                performance and maintainability.
+              </li>
+              <li>
+                Conducted unit testing using BDD (Behavior-Driven Development)
+                and performed system integration testing to ensure code quality,
+                application stability, and optimized performance.
+              </li>
+              <li>
+                Prepared release documentation and deployment scripts; provided
+                production support and issue resolution as required.
+              </li>
+              <li>
+                Actively participated in Agile workflows, collaborating closely
+                with Business Analysts, Product Owners, and QA teams to ensure
+                alignment with project goals and timely delivery.
+              </li>
+            </ul>
           </VerticalTimelineElement>
           {/* 2 */}
           <VerticalTimelineElement
@@ -77,12 +199,13 @@ const About = () => {
             contentStyle={{
               background: "rgb(244, 250, 255)",
               color: "black",
+              borderTop: "3px solid #4cc2e4",
             }}
             animate={true}
             contentArrowStyle={{ borderRight: "7px solid  rgb(254, 255, 255)" }}
             date={<span style={{ color: "#ffffff" }}>Feb 2020 – Dec 2020</span>}
-            iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-            icon={<OutsystemsIcon />}
+            iconStyle={{ background: "rgb(250, 250, 250)", color: "#fff" }}
+            icon={<AlvIcon />}
           >
             <h3 className="vertical-timeline-element-title">
               Service Engineer
@@ -90,6 +213,12 @@ const About = () => {
             <h4 className="vertical-timeline-element-subtitle">
               ALVTechnologies Philippine Incorporation
             </h4>
+            <ul className="list-disc pl-6 space-y-2 text-gray-600 mt-5">
+              <li>
+                Installed and programmed laboratory equipment for school,
+                hospital, and industrial applications.
+              </li>
+            </ul>
           </VerticalTimelineElement>
         </VerticalTimeline>
       </div>
--- src/util/Icons.jsx
+++ src/util/Icons.jsx
@@ -1,5 +1,15 @@
+import Alv from "../assets/alv.jpg";
 import Outsystems from "../assets/outsystems.png";
+import SevenSeven from "../assets/sevenseven.png";
 
 export const OutsystemsIcon = () => {
   return <img src={Outsystems} alt="OutSystems" className="rounded-full" />;
+};
+
+export const SevenSevenIcon = () => {
+  return <img src={SevenSeven} alt="SevenSeven" className="rounded-full" />;
+};
+
+export const AlvIcon = () => {
+  return <img src={Alv} alt="Alv" className="rounded-full mt-5" />;
 };
